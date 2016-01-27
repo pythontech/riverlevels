b2dffab54c78505b808db38d90dd3d0a470fde1c
Colin Hogben <colin@pythontech.co.uk>
2016-01-27 21:25:39 +0000

    Read configuration file

--- riverlevels.py
+++ riverlevels.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 #=======================================================================
-#       Get river levels from Environment Agency
+# Get river levels from Environment Agency
+#-----------------------------------------------------------------------
+# Copyright (C) 2016  Colin Hogben <colin@pythontech.co.uk>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
 #=======================================================================
 import os
 import logging
@@ -9,23 +26,13 @@ import json
 
 API_ROOT = 'http://environment.data.gov.uk/flood-monitoring'
 
-SAVEFILE = os.path.expanduser('~/.riverlevels.save')
-def my_monitors():
-    return (Monitor(name='Abingdon Lock',
-                    station='1503TH',qualifier='Downstream Stage',
-                    threshold=0.05),
-            Monitor(name='Ock at Tesco',
-                    station='1790TH',qualifier='Downstream Stage',
-                    threshold=0.05),
-            )
-
 _log = logging.getLogger('riverlevels')
 
 class Monitor(object):
-    def __init__(self, name, station, qualifier, threshold=0.1):
-        self.name = name
+    def __init__(self, station, qualifier, name=None, threshold=0.1):
         self.station = station
         self.qualifier = qualifier
+        self.name = name  or  station
         self.threshold = threshold
         self.key = '%s.%s' % (station, qualifier)
         self.alert_level = None
@@ -58,7 +65,7 @@ class Monitor(object):
                 if item['parameter'] == 'level':
                     latest = item['latestReading']
                     return latest['value'], latest['dateTime']
-        raise KeyError('No level measure for %s' % qualifier)
+        raise KeyError('No level measure for %s' % self.qualifier)
 
     def check_alert(self, value, date):
         if self.alert_level is None:
@@ -85,6 +92,24 @@ class Manager(object):
         except IOError as e:
             self.save = {}
 
+    @classmethod
+    def from_config(cls, config):
+        monitors = []
+        for mondef in config.get('monitors', []):
+            monitors.append(Monitor(**mondef))
+        savefile = config.get('savefile',
+                              os.path.expanduser('~/.riverlevels.save'))
+        return cls(monitors, savefile)
+
+    @classmethod
+    def from_config_file(cls, filespec):
+        if isinstance(filespec, file):
+            config = json.load(filespec)
+        else:
+            with open(filespec,'r') as f:
+                config = json.load(f)
+        return cls.from_config(config)
+
     def read_save(self):
         with open(self.savefile,'r') as f:
             data = f.read()
@@ -103,7 +128,11 @@ class Manager(object):
             json.dump(self.save, f, indent=2, sort_keys=True)
         os.rename(newsave, self.savefile)
 
-    def do_update(self):
+    def evaluate_alerts(self):
+        """Check level for each monitor and return alerts.
+        An alert is raised if the current level differs from the previously
+        alerted level by more than the configured threshold.
+        """
         alerts = []
         for mon in self.monitors:
             mon.from_save(self.save)
@@ -119,20 +148,6 @@ class Manager(object):
                     alerts.append(alert)
         return alerts
 
-def get_measures(station, qualifier='Stage'):
-    f = urllib2.urlopen(API_ROOT+'/id/measures?stationReference='+station)
-    body = f.read()
-    data = json.loads(body)
-    return data
-
-def get_level(station, qualifier='Stage'):
-    data = get_measures(station, qualifier)
-    for item in data['items']:
-        if item['qualifier'] == qualifier:
-            latest = item['latestReading']
-            return latest['value'], latest['dateTime']
-    raise KeyError('No measure for %s' % qualifier)
-
 if __name__=='__main__':
     import argparse
     ap = argparse.ArgumentParser()
@@ -145,24 +160,23 @@ if __name__=='__main__':
     # level [-q qual] stationref
     level = sub.add_parser('level')
     level.add_argument('-q','--qualifier', default='Stage',
-                    help='Select which measure (default: Stage)')
+                       help='Select which measure (default: Stage)')
     level.add_argument('station')
-    # update
-    update = sub.add_parser('update')
-    update.add_argument('-n','--noaction', action='store_true')
+    # alerts
+    alerts = sub.add_parser('alerts')
+    alerts.add_argument('-c','--config', type=argparse.FileType('r'),
+                        help='Configuration file (default: ~/.riverlevels.conf)')
     # --
     args = ap.parse_args()
     logging.basicConfig(level=args.loglevel)
     if args.action == 'level':
-        mon = Monitor(args.station, args.station, args.qualifier)
+        mon = Monitor(args.station, args.qualifier)
         value, date = mon.get_level()
         print value, date
-    elif args.action == 'update':
-        # do_update()
-        manager = Manager(my_monitors(), SAVEFILE)
-        alerts = manager.do_update()
+    elif args.action == 'alerts':
+        conffile = args.config or os.path.expanduser('~/.riverlevels.conf')
+        manager = Manager.from_config_file(conffile)
+        alerts = manager.evaluate_alerts()
         manager.write_save()
         for alert in alerts:
             print alert
-
-
